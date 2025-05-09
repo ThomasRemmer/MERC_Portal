fe4da777a25964a3571d11089df7832cd57263a1
ThomasRemmer <remmert765@gmail.com>
2025-05-09 11:15:11 +0100

    added desktop icon functionality

---
+++ components/desktopIcon.vue
@@ -0,0 +1,25 @@
+<template>
+    <img :src="icon" :style="iconStyle" @click="handleClick" />
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      icon: String,
+      iconStyle: Object,
+      targetPage: String, 
+    },
+    methods: {
+      handleClick() {
+        this.$emit("icon-click", this.targetPage);
+      },
+    },
+  };
+  </script>
+    <style scoped>
+    img {
+      width: 50px;
+      height: 50px;
+      cursor: pointer;
+    }
+    </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,64 @@
 <template>
   <div>
-    <PageContainer>
-      <RulesPage />
-    </PageContainer>
-    <PageContainer>
-      <CharactersPage />
-    </PageContainer>
-    <PageContainer>
-      <CharacterPage />
-    </PageContainer>
+
+    <div class="icons">
+      <desktop-icon icon="/icons/rules.svg" targetPage="rules" @icon-click="addPageContainer" />
+      <desktop-icon icon="/icons/file.svg" targetPage="characters" @icon-click="addPageContainer" />
+      <desktop-icon icon="/icons/user.svg" targetPage="character" @icon-click="addPageContainer" />
+    </div>
+
+
+    <div class="page-containers">
+      <PageContainer v-for="(container, index) in pageContainers" :key="index">
+        <component :is="getPageComponent(container.page)" />
+      </PageContainer>
+    </div>
   </div>
 </template>
 
 <script>
 import PageContainer from "@/components/PageContainer.vue";
+import DesktopIcon from "@/components/desktopIcon.vue";
+import { defineAsyncComponent } from "vue";
 
 export default {
   components: {
     PageContainer,
+    DesktopIcon,
     RulesPage: defineAsyncComponent(() => import("./rules.vue")),
     CharactersPage: defineAsyncComponent(() => import("./characters.vue")),
     CharacterPage: defineAsyncComponent(() => import("./character.vue")),
   },
+  data() {
+    return {
+      pageContainers: [], 
+    };
+  },
+  methods: {
+    addPageContainer(page) {
+      this.pageContainers.push({ page });
+    },
+    getPageComponent(page) {
+      const pageMap = {
+        rules: "RulesPage",
+        characters: "CharactersPage",
+        character: "CharacterPage",
+      };
+      return pageMap[page] || null;
+    },
+  },
 };
 </script>
+
+<style>
+.icons {
+  display: flex;
+  gap: 16px;
+  margin-bottom: 16px;
+}
+.page-containers {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+}
+</style>
